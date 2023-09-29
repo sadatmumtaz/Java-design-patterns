785356cca1f2f6bec9306be92455af83496cbedc
sadatmumtaz <106954465+sadatmumtaz@users.noreply.github.com>
2023-09-30 01:18:36 +0500

    Add files via upload
    facad

---
+++ DesignPatterns/src/GOF/facade/VideoFile/Demo.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package GOF.facade.VideoFile;
+
+/**
+ *
+ * @author user
+ */
+import java.io.File;
+
+public class Demo {
+    public static void main(String[] args) {
+        VideoConversionFacade converter = new VideoConversionFacade();
+        File mp4Video = converter.convertVideo("youtubevideo.ogg", "mp4");
+        // ...
+    }
+}
---
+++ DesignPatterns/src/GOF/facade/VideoFile/MPEG4CompressionCodec.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package GOF.facade.VideoFile;
+
+/**
+ *
+ * @author user
+ */
+public class MPEG4CompressionCodec implements Codec {
+    public String type = "mp4";
+
+}

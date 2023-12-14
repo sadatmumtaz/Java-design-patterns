0ab1146bcec086e3353dafaaa130f1a494b73c9b
sadatmumtaz <106954465+sadatmumtaz@users.noreply.github.com>
2023-12-14 09:31:05 +0500

    class diagram

---
+++ DesignPatterns/src/GOF/Memento/Rectangle.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package GOF.Memento;
+
+/**
+ *
+ * @author user
+ */
+import java.awt.*;
+
+public class Rectangle extends BaseShape {
+    private int width;
+    private int height;
+
+    public Rectangle(int x, int y, int width, int height, Color color) {
+        super(x, y, color);
+        this.width = width;
+        this.height = height;
+    }
+
+    @Override
+    public int getWidth() {
+        return width;
+    }
+
+    @Override
+    public int getHeight() {
+        return height;
+    }
+
+    @Override
+    public void paint(Graphics graphics) {
+        super.paint(graphics);
+        graphics.drawRect(x, y, getWidth() - 1, getHeight() - 1);
+    }
+}
